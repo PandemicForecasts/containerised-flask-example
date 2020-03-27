b887a90ed84e19d1ac9d04833242c2fd63d4c4e1
David Rowthorn <davidrowthorn@gmail.com>
2020-03-27 08:59:46 +0000

    PEP8 compliant

--- src/app.py
+++ src/app.py
@@ -2,9 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@app.route('/alive')
+def healthcheck():
     return "Alive!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
